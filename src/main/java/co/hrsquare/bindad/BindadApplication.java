e39b06bcc63e88aa46a35df1ede7b97f0f0ff436
Anurag Ruhela <anurag@hrsquare.co>
2020-12-22 12:37:00 +0000

    Initial Commit

---
+++ src/main/java/co/hrsquare/bindad/BindadApplication.java
@@ -0,0 +1,13 @@
+package co.hrsquare.bindad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BindadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BindadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/hrsquare/bindad/BindadApplicationTests.java
@@ -0,0 +1,13 @@
+package co.hrsquare.bindad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BindadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
