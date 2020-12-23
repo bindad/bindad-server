18ebfb32fc5d625aa5be819086f3e9ebede3f254
Anurag Ruhela <anurag@hrsquare.co>
2020-12-23 20:34:10 +0000

    Local MySql DB connectivity

--- src/main/java/co/hrsquare/bindad/controller/HelloController.java
+++ src/main/java/co/hrsquare/bindad/controller/HelloController.java
@@ -1,14 +1,23 @@
 package co.hrsquare.bindad.controller;
 
+import co.hrsquare.bindad.mapper.ITestMapper;
+import co.hrsquare.bindad.model.TestModel;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private final ITestMapper testMapper;
+
+    public HelloController(ITestMapper testMapper) {
+        this.testMapper = testMapper;
+    }
+
     @RequestMapping("/api/hello")
     public String index() {
-        return "Hello Bindad!";
+        TestModel testModel = testMapper.findById("1");
+        return "Hello " + testModel.getName() + "!";
     }
 
 }
